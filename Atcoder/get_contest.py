2b3776347cf9c2ca83391e68b5017ca9a548e5a9
Алексей Михненко <alex@MacBook-Pro-Aleksej-2.local>
2023-03-16 18:48:09 +0300

    Added atcoder parser.

---
+++ Atcoder/get_contest.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+from Library.helpers import set_language, get_html_code
+from Atcoder.get_problem import parse_problem_from_html
+from Library.default_classes import Contest
+
+
+def parse_contest_from_html(html_code, link=None):
+    soup = BeautifulSoup(html_code, 'html.parser')
+    title_node = soup.find('title')
+    contest_title = None if title_node is None else title_node.text
+
+    problems = []
+    for problem in soup.find_all('span', 'lang-en'):
+        problems.append(parse_problem_from_html(str(problem)))
+
+    for index, title in enumerate(soup.find_all('span', {'class': 'h2'})):
+        problems[index].title = title.text[4:]
+        problems[index].index = title.text[0]
+
+    return Contest(title=contest_title, link=link, problems=problems)
+
+
+def get_contest(url, use_selenium=False):
+    html_code = get_html_code(set_language(url, 'lang', 'en'), use_selenium=use_selenium)
+    if html_code is None:
+        return None
+
+    return parse_contest_from_html(html_code, link=url)
+
+
+def main():
+    url = input('Input the link to the problems: ')
+    contest = get_contest(url, use_selenium=False)
+    if contest is None:
+        print('Failed to load problems.')
+        sys.exit(0)
+
+    print(contest)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Atcoder/get_problem.py
@@ -0,0 +1,55 @@
+import re
+from bs4 import BeautifulSoup
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+from Library.helpers import set_language, get_html_code
+from Library.default_classes import Problem
+
+
+def parse_problem_from_html(html_code, link=None):
+    soup = BeautifulSoup(html_code, 'html.parser')
+
+    title_node = soup.find('title')
+    problem_title = None
+    problem_index = None
+    if title_node is not None:
+        problem_title = title_node.text[4:]
+        problem_index = title_node.text[0]
+
+    regex_input = re.compile(r'Sample Input [0-9]+')
+    regex_output = re.compile(r'Sample Output [0-9]+')
+    inputs = []
+    outputs = []
+
+    for root_node in soup.find_all('div', {'class': 'part'}):
+        title = root_node.find('h3').text
+        if regex_input.match(title):
+            inputs.append(root_node.find('pre').text)
+        elif regex_output.match(title):
+            outputs.append(root_node.find('pre').text)
+
+    return Problem(title=problem_title, index=problem_index, link=link, inputs=inputs, outputs=outputs)
+
+
+def get_problem(url, use_selenium=False):
+    html_code = get_html_code(set_language(url, 'lang', 'en'), use_selenium=use_selenium)
+    if html_code is None:
+        return None
+
+    return parse_problem_from_html(html_code, link=url)
+
+
+def main():
+    url = input('Input the link to the problem: ')
+    problem = get_problem(url, use_selenium=False)
+    if problem is None:
+        print('Failed to load tests.')
+        sys.exit(0)
+
+    print(problem)
+
+
+if __name__ == '__main__':
+    main()
