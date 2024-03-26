034255953e02a8c4ae8637d8738ef4f05105577c
Михненко Алексей Андреевич <aamikhnenko_1@edu.hse.ru>
2024-03-26 19:45:20 +0300

    Bug fix in atcoder get_problem.py

--- cpscripts/atcoder/get_problem.py
+++ cpscripts/atcoder/get_problem.py
@@ -11,10 +11,10 @@ def parseProblemFromHtml(html: str, link: str=None) -> Problem:
     soup = BeautifulSoup(html, 'html.parser')
 
     titleNode = soup.find('title')
-    title = None
+    problemTitle = None
     index = None
     if titleNode is not None:
-        index, title = getProblemIndexAndTitle(titleNode.text)
+        index, problemTitle = getProblemIndexAndTitle(titleNode.text)
 
     inputRegex = re.compile(r'Sample Input [0-9]+')
     outputRegex = re.compile(r'Sample Output [0-9]+')
@@ -28,4 +28,4 @@ def parseProblemFromHtml(html: str, link: str=None) -> Problem:
         elif outputRegex.match(title):
             outputs.append(rootNode.find('pre').text)
 
-    return Problem(title=title, index=index, link=link, inputs=inputs, outputs=outputs)
+    return Problem(title=problemTitle, index=index, link=link, inputs=inputs, outputs=outputs)
